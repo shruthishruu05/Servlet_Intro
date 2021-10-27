bdae6c4d88a5a787893e5d25c5c90a5f0712f5d3
shruthishruu05 <shruthi.m@perfios.com>
2021-10-27 23:37:57 +0530

    ADDED : Login with predefined user

---
+++ src/main/java/com/myfirstservlet/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.myfirstservlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = { "/LoginServlet" },
+        initParams = {
+                @WebInitParam(name = "user", value = "Shruthi"),
+                @WebInitParam(name = "password", value = "BridgeLabz")
+        }
+)
+public class LoginServlet  extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+
+        //get request parameters for user ID and password
+        String user = req.getParameter("user");
+        String pwd = req.getParameter("pwd");
+
+        // get servlet config init params
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+        if(userID.equals(user) && password.equals(pwd)) {
+            req.setAttribute("user",user);
+            req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out  = resp.getWriter();
+            out.println("<font color = red> Either username or password is wrong</font>");
+            rd.include(req, resp);
+        }
+
+    }
+}
